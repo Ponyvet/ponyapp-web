90e865a82131c673f6e9563981bb84896796c343
Vladimir Azpeitia <vazpeitiah@gmail.com>
2025-12-31 17:14:28 -0600

    feat: enhance login page with authentication check and navigation

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -1,11 +1,23 @@
-import { LoginForm } from "@/features/auth/components/LoginForm";
+import { LoginForm } from '@/features/auth/components/LoginForm'
+import { useEffect } from 'react'
+import { useAuthStore } from '../store/authStore'
+import { useNavigate } from 'react-router'
 
 export default function LoginPage() {
+  const navigate = useNavigate()
+  const { isAuth } = useAuthStore()
+
+  useEffect(() => {
+    if (isAuth) {
+      navigate('/')
+    }
+  }, [isAuth, navigate])
+
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-4xl">
         <LoginForm />
       </div>
     </div>
-  );
+  )
 }
