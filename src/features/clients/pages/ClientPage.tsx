c5dfeff473d60e422de27545753bf38babeaaf11
Vladimir Azpeitia <vazpeitiah@gmail.com>
2026-01-24 16:03:00 -0600

    feat: remove search functionality from ClientPage and update DataTable to display all clients

--- src/features/clients/pages/ClientPage.tsx
+++ src/features/clients/pages/ClientPage.tsx
@@ -1,42 +1,18 @@
-import {
-  InputGroup,
-  InputGroupAddon,
-  InputGroupInput,
-} from '@/components/ui/input-group'
 import useGetClients from '../queries/useGetClients'
 
 import { columns } from '@/features/clients/components/clients/Columns'
 import { DataTable } from '@/shared/components/DataTable'
 import { Button } from '@/components/ui/button'
-import { PlusIcon, Search } from 'lucide-react'
-import { useMemo, useState } from 'react'
+import { PlusIcon } from 'lucide-react'
 import { useNavigate } from 'react-router'
 
 const ClientPage = () => {
   const navigate = useNavigate()
-  const [searchTerm, setSearchTerm] = useState('')
   const { data: clients, isSuccess } = useGetClients()
-  const filteredClients = useMemo(() => {
-    if (!isSuccess) return []
-    return clients.filter((client) =>
-      client.name.toLowerCase().includes(searchTerm.toLowerCase()),
-    )
-  }, [clients, isSuccess, searchTerm])
 
   return (
     <div>
       <div className="flex items-center justify-end gap-4 pb-4">
-        <InputGroup className="max-w-xs">
-          <InputGroupInput
-            placeholder="Buscar..."
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <InputGroupAddon>
-            <Search />
-          </InputGroupAddon>
-          {/* <InputGroupAddon align="inline-end">12 results</InputGroupAddon> */}
-        </InputGroup>
         <Button
           variant="outline"
           size="sm"
@@ -46,7 +22,7 @@ const ClientPage = () => {
           Agregar Cliente
         </Button>
       </div>
-      {isSuccess && <DataTable columns={columns} data={filteredClients} />}
+      {isSuccess && <DataTable columns={columns} data={clients} />}
     </div>
   )
 }
