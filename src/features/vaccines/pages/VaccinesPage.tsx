fd0a51a9d1cd394fec8a395f965abd4837c1368f
Vladimir Azpeitia <vazpeitiah@gmail.com>
2026-01-24 21:05:13 -0600

    feat: refactor VaccinesPage to use Card components for layout; enhance structure and improve empty state display

--- src/features/vaccines/pages/VaccinesPage.tsx
+++ src/features/vaccines/pages/VaccinesPage.tsx
@@ -5,25 +5,43 @@ import { DataTable } from '@/shared/components/DataTable'
 import { Button } from '@/components/ui/button'
 import { useNavigate } from 'react-router'
 import useGetVaccines from '../queries/useGetVaccines'
+import { Card, CardAction, CardContent, CardHeader } from '@/components/ui/card'
+import EmptyTable from '@/shared/components/EmptyTable'
 
 const VaccinesPage = () => {
   const navigate = useNavigate()
   const { data: vaccines = [] } = useGetVaccines()
 
   return (
-    <div>
-      <div className="flex items-center justify-end gap-4 pb-4">
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => navigate('/vaccines/add')}
-        >
-          <PlusIcon />
-          Agregar Vacuna
-        </Button>
-      </div>
-      <DataTable columns={columns} data={vaccines} />
-    </div>
+    <Card className="border-none shadow-none">
+      <CardHeader>
+        <h2 className="text-2xl font-bold">Vacunas</h2>
+        <CardAction>
+          <Button size="sm" onClick={() => navigate('/vaccines/add')}>
+            <PlusIcon />
+            Agregar Vacuna
+          </Button>
+        </CardAction>
+      </CardHeader>
+      <CardContent>
+        {vaccines.length > 0 ? (
+          <DataTable columns={columns} data={vaccines} />
+        ) : (
+          <EmptyTable
+            icon={<PlusIcon />}
+            title="No hay vacunas aún"
+            description="Agrega tu primera vacuna para comenzar a gestionar las aplicaciones."
+            buttonText={
+              <>
+                <PlusIcon className="mr-2" />
+                Agregar primera vacuna
+              </>
+            }
+            onclick={() => navigate('/vaccines/add')}
+          />
+        )}
+      </CardContent>
+    </Card>
   )
 }
 
